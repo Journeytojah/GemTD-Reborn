c5c101ab39adf69937de86a1e459364f4fc7fefb
Alexandru Higyedi <alexkidoo@gmail.com>
2023-04-27 23:33:56 +0700

    Another day, another failed attempt at pathfinding

--- src/game/Engine.ts
+++ src/game/Engine.ts
@@ -34,14 +34,13 @@ export class Engine {
 var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
   // This creates a basic Babylon Scene object (non-mesh)
   var scene = new BABYLON.Scene(engine);
+
   window.CANNON = cannon;
   scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.CannonJSPlugin());
   // Create a top-down camera, position it above the floor, pointing down at the origin
   var camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, 0, 23, BABYLON.Vector3.Zero(), scene);
 
   camera.setPosition(new BABYLON.Vector3(0, 64, 0));
-
-
 
   // This targets the camera to scene origin
   camera.setTarget(BABYLON.Vector3.Zero());
@@ -61,20 +60,9 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
   // world manager
   const worldManager = new WorldManager(scene);
 
+
   scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.CannonJSPlugin());
 
-  // drop a sphere in the middle of the world with physics enabled and collision detection
-  const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-  sphere.position.y = 10;
-  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
-
-  // create a ground plane with physics enabled and collision detection
-  // const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, scene);
-  // ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
-
-
-
-
-
   return scene;
+
 };
--- src/game/world.ts
+++ src/game/world.ts
@@ -1,5 +1,5 @@
 import * as BABYLON from 'babylonjs';
-
+import * as PF from 'pathfinding';
 class Tile {
   public walkable: boolean = false;
   // public tower: Entity | null = null;
@@ -9,6 +9,7 @@ class Tile {
 
 export class Board {
   public tiles: Tile[][] = [];
+  public waypoints: Tile[] = [];
 
   constructor(public width: number, public height: number) {
     for (let x = 0; x < width; x++) {
@@ -21,23 +22,120 @@ export class Board {
   }
 
   public generateLevel() {
+    // store the waypoints in an array for easy access
+    const waypoints: Tile[] = [];
+
+    // generate a path from 39,5
+    let currentTile: Tile = this.tiles[39][5];
+    waypoints.push(currentTile);
+    currentTile.walkable = true;
+
+    // generate a path to 26,5
+    while (currentTile.x > 26) {
+      currentTile = this.tiles[currentTile.x - 1][5];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    // generate a path to 26,16
+    while (currentTile.y < 21) {
+      currentTile = this.tiles[26][currentTile.y + 1];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    // generate a path to 7,21
+    while (currentTile.x > 7) {
+      currentTile = this.tiles[currentTile.x - 1][21];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    // generate a path to 8,5
+    while (currentTile.y > 5) {
+      currentTile = this.tiles[7][currentTile.y - 1];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    // generate a path to 18,5
+    while (currentTile.x < 18) {
+      currentTile = this.tiles[currentTile.x + 1][5];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    // generate a path to 18,28
+    while (currentTile.y < 28) {
+      currentTile = this.tiles[18][currentTile.y + 1];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    // generate a path to 0,31
+    while (currentTile.x > 0) {
+      currentTile = this.tiles[currentTile.x - 1][28];
+      waypoints.push(currentTile);
+      currentTile.walkable = true;
+    }
+
+    this.waypoints = waypoints;
+    return waypoints;
+  }
+
+  public spawnEnemy(scene: BABYLON.Scene) {
+    const enemy = BABYLON.MeshBuilder.CreateBox("enemy", { width: 0.8, height: 1 }, scene);
+    enemy.position.x = this.waypoints[0].x - this.width / 2;
+    enemy.position.y = 0.65;
+    enemy.position.z = this.waypoints[0].y - this.height / 2;
+    enemy.material = new BABYLON.StandardMaterial("enemy", scene);
+    enemy.material.diffuseColor = new BABYLON.Color3(1, 1, 0);
+    enemy.physicsImpostor = new BABYLON.PhysicsImpostor(enemy, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0.4, restitution: 0.9 }, scene);
+  }
+
+  // create a loop that updates the enemy's position each frame to follow the waypoints
+  public updateEnemy(scene: BABYLON.Scene) {
+    scene.registerBeforeRender(() => {
+      const enemy = scene.getMeshByName("enemy");
+      if (enemy) {
+        const enemyPosition = enemy.position;
+        const waypoint = this.waypoints[0];
+        const waypointPosition = new BABYLON.Vector3(waypoint.x - this.width / 2, 0.1, waypoint.y - this.height / 2);
+        const distance = BABYLON.Vector3.Distance(enemyPosition, waypointPosition);
+        if (distance < 0.1) {
+          this.waypoints.shift();
+        }
+      }
+    });
+  }
+
+  // move the enemy in a straight line to the right for now
+  public moveEnemy(scene: BABYLON.Scene) {
+    const enemy = scene.getMeshByName("enemy");
+    // console.log(enemy);
+
+    var grid = new PF.Grid(this.width, this.height);
+    // make a loop and set the walkable tiles to true
     for (let x = 0; x < this.width; x++) {
-      this.tiles[x][0].walkable = true;
-      this.tiles[x][this.height - 1].walkable = true;
-    }
-    for (let y = 0; y < this.height; y++) {
-      this.tiles[0][y].walkable = true;
-      this.tiles[this.width - 1][y].walkable = true;
+      for (let y = 0; y < this.height; y++) {
+        const tile = this.tiles[x][y];
+        if (tile.walkable) {
+          grid.setWalkableAt(x, y, true);
+        }
+      }
     }
 
+    // create a finder
+    var finder = new PF.AStarFinder();
+
+    scene.registerBeforeRender(() => {
+      if (enemy) {
+
+      }
+    });
   }
 
-  public getTile(x: number, y: number): Tile | null {
-    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
-      return null;
-    }
-    return this.tiles[x][y];
-  }
+
 
   // create a mesh for each tile
   public createMeshes(scene: BABYLON.Scene) {
@@ -47,7 +145,7 @@ export class Board {
     for (let x = 0; x < this.width; x++) {
       for (let y = 0; y < this.height; y++) {
         const tile = this.tiles[x][y];
-        const tileMesh = BABYLON.MeshBuilder.CreateBox(`tile-${x}-${y}`, { width: 1, height: 1 }, scene);
+        const tileMesh = BABYLON.MeshBuilder.CreateBox(`tile-${x}-${y}`, { width: 1, height: 0.1 }, scene);
         tileMesh.position.x = x;
         tileMesh.position.y = 0;
         tileMesh.position.z = y;
@@ -63,24 +161,23 @@ export class Board {
         tileMesh.position.x -= this.width / 2;
         tileMesh.position.z -= this.height / 2;
 
-
         // enable physics
         tileMesh.physicsImpostor = new BABYLON.PhysicsImpostor(tileMesh, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
       }
     }
 
-
-
   }
+
 
   // log the tile being clicked on to the console
   public addEventManager(scene: BABYLON.Scene) {
     scene.onPointerObservable.add((pointerInfo) => {
       if (pointerInfo.type === BABYLON.PointerEventTypes.POINTERDOWN) {
         const pickResult = scene.pick(scene.pointerX, scene.pointerY);
-        if (pickResult.hit) {
-          console.log(pickResult.pickedMesh?.name);
+        if (pickResult?.hit) {
+          console.log(scene.pointerX, scene.pointerY);
+          console.log(pickResult?.pickedMesh?.name);
         }
       }
     });
@@ -92,9 +189,12 @@ export class WorldManager {
 
   constructor(scene: BABYLON.Scene) {
     this.scene = scene;
-    this.board = new Board(24, 24);
+    this.board = new Board(40, 40);
     this.board.generateLevel();
     this.board.createMeshes(scene);
     this.board.addEventManager(scene);
+    this.board.spawnEnemy(scene);
+    this.board.moveEnemy(scene);
   }
 }
+
